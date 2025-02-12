ee31d46cd97859ba19bce30bc10c180630ebd145
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-12 17:22:19 -0600

    Initial Commit

---
+++ AppHidingAndRevealingObjects/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppHidingAndRevealingObjects
+//
+//  Created by Miriam Sanchez on 12/02/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AppHidingAndRevealingObjects/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AppHidingAndRevealingObjects
+//
+//  Created by Miriam Sanchez on 12/02/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
