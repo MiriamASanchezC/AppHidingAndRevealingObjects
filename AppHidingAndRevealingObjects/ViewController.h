45efab0fd1a19d2f4ecf4eb4bc91dacea8033565
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-19 21:29:55 -0600

    AppHidingAndRevealing

--- AppHidingAndRevealingObjects/ViewController.h
+++ AppHidingAndRevealingObjects/ViewController.h
@@ -9,6 +9,17 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segControl;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+
+@property (weak, nonatomic) IBOutlet UISwitch *switch1;
+
+- (IBAction)showObjects:(id)sender;
+- (IBAction)hideObjects:(id)sender;
+
+- (IBAction)amihidden:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
--- AppHidingAndRevealingObjects/ViewController.m
+++ AppHidingAndRevealingObjects/ViewController.m
@@ -19,4 +19,25 @@
 }
 
 
+- (IBAction)amihidden:(id)sender {
+    if(self.slider.hidden == YES){
+        self.label.text = @"The objects are hidden";
+    }else{
+        self.label.text = @"The objects are not hidden";
+    }
+}
+
+- (IBAction)hideObjects:(id)sender {
+    self.segControl.hidden = YES;
+    self.textField.hidden = YES;
+    self.slider.hidden = YES;
+    self.switch1.hidden = YES;
+}
+
+- (IBAction)showObjects:(id)sender {
+    self.segControl.hidden = NO;
+    self.textField.hidden = NO;
+    self.slider.hidden = NO;
+    self.switch1.hidden = NO;
+}
 @end
